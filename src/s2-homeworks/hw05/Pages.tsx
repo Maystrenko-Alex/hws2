d9a17f6c4c0d696aff17620c64eda90e0566ae9a
Alex Maystrenko <maystrenko_alex@mail.ru>
2022-10-17 03:39:33 +0300

    hw5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -8,7 +8,7 @@ import JuniorPlus from './pages/JuniorPlus'
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+    JUNIOR_PLUS: '/juniorPlus',
 }
 
 function Pages() {
@@ -17,7 +17,7 @@ function Pages() {
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
                 {/*роутинг будут писать студенты*/}
-                <Route path='/' element={<PreJunior/>}/>
+                <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
